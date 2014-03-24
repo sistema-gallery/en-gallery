5fedc82a0ce02b93ec146b4f6c6367ca2569f393
Dmitry Poddubny <css.experts@gmail.com>
2014-03-24 17:16:58 +0300

    init site structure

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (grunt) {
+    require('time-grunt')(grunt)
+    require('load-grunt-config')(grunt, {
+        loadGruntTasks: {
+            pattern: ['grunt-contrib-*', 'assemble']
+        }
+    })
+}
